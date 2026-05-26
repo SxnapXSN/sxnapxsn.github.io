037112e5fb59b436073c32d9eef411af503e3d22
SxnapXSN <SxnapXSN@users.noreply.github.com>
2026-05-26 18:23:28 +0700

    Switch GitHub Pages site to root domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === "build" && process.env.GITHUB_ACTIONS ? "/XSNIS/" : "/",
+  base: command === "build" && process.env.GITHUB_ACTIONS ? "/" : "/",
   server: {
     host: "127.0.0.1",
     port: 5199
