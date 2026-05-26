f5e092c5b9364c0ff69ec7493ae306c54c65f209
SxnapXSN <SxnapXSN@users.noreply.github.com>
2026-05-26 23:11:59 +0700

    build to docs folder

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command }) => ({
   plugins: [react()],
   base: command === "build" ? "/XSNIS.github.io/" : "/",
+  build: {
+    outDir: "docs"
+  },
   server: {
     host: "127.0.0.1",
     port: 5199
